23a6a567fa0f9a30919ea21daace597701d2838e
syeda <sbtshakeel61@gmail.com>
2024-05-31 05:15:24 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+//never type 
+
+let abc : string & number = 10 //error because never haven't make any type ! never 
+    
+abc =  "string"  //error
+
+
+let xyz : boolean & undefined = true //error
+
+xyz = undefined //error
